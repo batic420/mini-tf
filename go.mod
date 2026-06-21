d644b1423e003036b10b28c25dc3adb300268bd6
batic420 <batic420@users.noreply.github.com>
2026-06-21 11:28:32 +0200

    feat: add Load() function and contents for Envelope

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/batic420/mini-tf
 
 go 1.26.4
+
+require github.com/goccy/go-yaml v1.19.2
--- internal/decoder/decoder.go
+++ internal/decoder/decoder.go
@@ -1,6 +1,16 @@
 package decoder
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/goccy/go-yaml"
+)
+
 type Envelope struct {
+	Parameters []Parameter `yaml:"parameters"`
+	Variables  Variables   `yaml:"variables"`
+	Resources  []Resource  `yaml:"resources"`
 }
 
 type Parameter struct {
@@ -30,3 +40,23 @@ type ResourceProperties struct {
 type ResourceOutputs struct {
 	Path string `yaml:"path"`
 }
+
+func Load(path string) (*Envelope, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("opening config: %w\n", err)
+	}
+	defer f.Close()
+
+	dec := yaml.NewDecoder(f, yaml.Strict())
+
+	var env Envelope
+	if err := dec.Decode(&env); err != nil {
+		return nil, fmt.Errorf("parsing config: %w\n", err)
+	}
+
+	return &env, nil
+}
+
+// TODO:
+// - Add pointer receiver for validation of `env`
