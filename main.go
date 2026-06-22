b90776dc6b58d26fcaca14d2c327b4b34c7db8dc
batic420 <batic420@users.noreply.github.com>
2026-06-22 22:35:21 +0200

    feat: add main.go to repository for first runs

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/batic420/mini-tf/internal/decoder"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("usage: mini-tf <path-to-yaml>")
+	}
+
+	env, err := decoder.Load(os.Args[1])
+	if err != nil {
+		log.Fatalf("failed to load config: %v", err)
+	}
+
+	fmt.Printf("%+v\n", env)
+}
